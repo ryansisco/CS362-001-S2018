1d3a0db805ec0de9eaf5986d9208698379f1fe15
ryansisco <ryansisco8@gmail.com>
2018-05-18 13:39:23 -0700

    tests ready to be editted

---
+++ projects/siscor/Assignment-4/Calendar/src/test/java/calendar/CalDayRandomTest.java
@@ -0,0 +1,28 @@
+package calendar;
+
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Random Test Generator  for CalDay class.
+ */
+
+public class CalDayRandomTest {
+	
+    /**
+     * Generate Random Tests that tests CalDay Class.
+     */
+	 @Test 	//deleteAppt()
+	  public void radnomtest()  throws Throwable  {
+		
+	 }
+	 @Test 	//getApptRange()
+	  public void radnomtest2()  throws Throwable  {
+		 
+	 }
+}
---
+++ projects/siscor/Assignment-4/Calendar/src/test/java/calendar/DataHandlerRandomTest.java
@@ -0,0 +1,29 @@
+package calendar;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Random Test Generator  for DataHandler class.
+ */
+
+public class DataHandlerRandomTest {
+	
+    /**
+     * Generate Random Tests that tests DataHandler Class.
+     */
+	 @Test 	//addAppt
+	  public void radnomtest()  throws Throwable  {
+		 
+	 }
+
+
+	
+}
